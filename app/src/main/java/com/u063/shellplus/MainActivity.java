1276df917a74de01b7a4bd14c02e85a7599c305a
mtkboy1 <mrbrawler42@gmail.com>
2025-03-16 22:41:06 +0300

    CREATED

--- app/src/main/java/com/u063/shellplus/MainActivity.java
+++ app/src/main/java/com/u063/shellplus/MainActivity.java
@@ -1,14 +1,18 @@
 package com.u063.shellplus;
 
 
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -19,7 +23,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
-
+    Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,22 +32,33 @@ public class MainActivity extends AppCompatActivity {
     }
     public void executeButton(View view){
         EditText etx = findViewById(R.id.shell_field);
-        TextView tx = new TextView(this);
+        Context c = this;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String result = new Shell(etx.getText().toString()).execute();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String cmd = etx.getText().toString();
+                        TextView tx = new TextView(c);
+                        tx.setTextSize(25);
+                        tx.setTextColor(Color.rgb(1,100,150));
+                        tx.setText(cmd);
 
-        String cmd = etx.getText().toString();
+                        LinearLayout ll = findViewById(R.id.scroll);
+                        ll.addView(tx);
 
-        tx.setTextSize(25);
-        tx.setTextColor(Color.rgb(1,100,150));
-        tx.setText(cmd);
+                        tx = new TextView(c);
+                        tx.setTextSize(25);
+                        tx.setTextColor(Color.BLACK);
+                        tx.setText(new Shell(etx.getText().toString()).execute());
 
-        LinearLayout ll = findViewById(R.id.scroll);
-        ll.addView(tx);
+                        ll.addView(tx);
+                    }
+                });
+            }
+        });
 
-        tx = new TextView(this);
-        tx.setTextSize(25);
-        tx.setTextColor(Color.BLACK);
-        tx.setText(new Shell(etx.getText().toString()).execute());
-
-        ll.addView(tx);
     }
 }
